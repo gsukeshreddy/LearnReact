ca1996d73dea7c1cf102435da9495d26e5129a73
Sukesh Reddy Gangumalla <gsukeshreddy13@hotmail.com>
2025-06-28 17:23:07 -0400

    Modified the docker compose file

--- learn-react-project/src/components/Navbar.tsx
+++ learn-react-project/src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const handleClearSearch = () => {
     return <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid px-3">
-  <a className="navbar-brand" href="#" style={{color: "dodgerblue" }}>Movies React</a>
+  <a className="navbar-brand" href="#" style={{color: "dodgerblue" }}>MovieFlix</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
